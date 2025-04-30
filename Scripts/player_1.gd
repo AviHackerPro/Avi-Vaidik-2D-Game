517452d2e96aab884850c5a8062b614eb2047878
GAMECHANGER01725 <vaidik.patel1725@gmail.com>
2025-04-30 18:51:26 +1000

    added animations for player

--- Scripts/player_1.gd
+++ Scripts/player_1.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
+@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -600.0
-
-@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,7 +14,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		$JumpSound.play()
+		jump_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -24,4 +24,10 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if Input.is_action_just_pressed("JUMP"):
+		animated_sprite.play("JUMP")
+	elif Input.is_action_just_pressed("Left") or Input.is_action_pressed("Right"):
+		animated_sprite.play("RUN")
+	else:
+		animated_sprite.play("IDLE")
 	move_and_slide()
