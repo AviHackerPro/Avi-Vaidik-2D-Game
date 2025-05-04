429374d5c2dee685bdb15ae3a033d23a635065f3
GAMECHANGER01725 <vaidik.patel1725@gmail.com>
2025-05-04 19:07:38 +1000

    added background music. Added coin sound. Added new background for outside the camera in Assets folder. Increased zoom. Added camera limits(need to fix)

--- Scripts/coin_script.gd
+++ Scripts/coin_script.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body):
 	if (body.name == "Player1"):
-		queue_free()
+		animation_player.play("coin_pickup")
 		game_manager.add_point()
