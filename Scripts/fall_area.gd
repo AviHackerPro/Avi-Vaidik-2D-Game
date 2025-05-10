8ed5ed9811e9a40cfabb535b4c8e9dab2bcc1214
GAMECHANGER01725 <vaidik.patel1725@gmail.com>
2025-05-10 13:40:54 +1000

    I added Buoyancy back. I couldn't do anything else. Tried to fix bugs but failed

--- Scripts/fall_area.gd
+++ Scripts/fall_area.gd
@@ -1,5 +1,6 @@
 extends Area2D
+@onready var game_manager: Node = %GameManager
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "Player1"):
-		get_tree().reload_current_scene()
+		game_manager.decrease_health()
